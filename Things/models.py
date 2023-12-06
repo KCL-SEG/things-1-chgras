333848010c07d5a0f865e6d9f42a17e00aef504b
qingshan.wen <qingshan.wen@kcl.ac.uk>
2023-12-06 17:12:59 +0000

    Create a basic web things

---
+++ Things/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.db.models import Model
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+# Create your models here.
+class Thing(Model):
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True)
+    quantity = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
